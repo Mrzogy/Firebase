f7131fbcdd44ceb724c733c29fb1a5e2f5a5e01d
mrzogy <magbolmrzogy@gmail.com>
2023-05-23 01:08:54 +0300

    Initial Commit

---
+++ Firebase/FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseApp.swift
+//  Firebase
+//
+//  Created by 3bood on 03/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
